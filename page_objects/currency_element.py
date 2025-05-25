b424facab12844a187b95ba5f0ffd59f37d242cc
Stepan <artamonov.stepan2020@yandex.ru>
2025-05-25 15:05:17 +0300

    Домашняя работа по отчётам в Allure

--- page_objects/currency_element.py
+++ page_objects/currency_element.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
@@ -14,6 +15,7 @@ class CurrencyElement:
         self.browser = browser
         self.wait = wait
 
+    @allure.step("Меняю валюту")
     def change_currency(self, currency_code):
         dropdown = self.wait.until(
             EC.presence_of_element_located(self.CURRENCY_DROPDOWN)
@@ -30,10 +32,13 @@ class CurrencyElement:
 
         if currency_code == "USD":
             option = self.wait.until(EC.element_to_be_clickable(self.USD_OPTION))
+            allure.attach("Поменяли валюту на USD")
         elif currency_code == "EUR":
             option = self.wait.until(EC.element_to_be_clickable(self.EUR_OPTION))
+            allure.attach("Поменяли валюту на EUR")
         elif currency_code == "GBP":
             option = self.wait.until(EC.element_to_be_clickable(self.GBP_OPTION))
+            allure.attach("Поменяли валюту на GBP")
         else:
             raise ValueError(f"Invalid currency code: {currency_code}")
 
@@ -45,6 +50,7 @@ class CurrencyElement:
 
         self.wait.until(EC.presence_of_element_located(self.SYMBOL_ELEMENT))
 
+    @allure.step("Получаю символ текущей валюты")
     def get_current_currency_symbol(self):
         return self.wait.until(
             EC.visibility_of_element_located(self.SYMBOL_ELEMENT)
--- page_objects/registration_page.py
+++ page_objects/registration_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.by import By
 from page_objects.base_page import BasePage
 
@@ -12,49 +13,65 @@ class RegistrationPage(BasePage):
     REGISTER_BUTTON = (By.XPATH, "//button[contains(text(), 'Continue')]")
     REGISTER_HEAD = (By.XPATH, '//h1[text()="Register Account"]')
     REGISTER_BODY = (By.ID, "form-register")
-
     SHOP_CART_LINK = (By.PARTIAL_LINK_TEXT, "Shopping Cart")
 
+    @allure.step("Открываю страницу 'Регистрации'")
     def open_register_page(self, base_url):
         self.browser.get(f"{base_url}/index.php?route=account/register")
+        allure.attach("Открыли страницу 'Регистрации'")
 
+    @allure.step("Ищу заголовок браузера")
     def find_browser_title(self):
         return self.browser.title == self.TITLE_TEXT
 
+    @allure.step("Ищу заголовок карточки регистрации")
     def find_card_name(self):
         return self.get_element(self.REGISTER_HEAD)
 
+    @allure.step("Ищу тело карточки регистрации")
     def find_card_body(self):
         return self.get_element(self.REGISTER_BODY)
 
+    @allure.step("Ищу ссылку на 'Shopping Cart'")
     def find_link(self):
         return self.get_element(self.SHOP_CART_LINK)
 
+    @allure.step("Ищу поле 'Пароль'")
     def find_password_field(self):
         return self.get_element(self.PASSWORD_INPUT)
 
+    @allure.step("Ищу POLICY_CHECK")
     def find_check_box(self):
         return self.get_element(self.POLICY_CHECK)
 
+    @allure.step("Ввожу данные в поле Имя")
     def enter_firstname(self, firstname):
         self.input_value(self.FN_INPUT, firstname)
 
+    @allure.step("Ввожу данные в поле Фамилия")
     def enter_lastname(self, lastname):
         self.input_value(self.LN_INPUT, lastname)
 
+    @allure.step("Ввожу данные в поле Email")
     def enter_email(self, email):
         self.input_value(self.EMAIL_INPUT, email)
 
+    @allure.step("Ввожу данные в поле Пароль")
     def enter_password(self, password):
         self.input_value(self.PASSWORD_INPUT, password)
 
+    @allure.step("Соглашаюсь с Privacy Policy")
     def agree_to_policy(self):
         checkbox = self.get_element(self.POLICY_CHECK)
         if not checkbox.is_selected():
             checkbox.click()
+            allure.attach("Активировал чек-бокс Privacy Policy")
 
+    @allure.step("Нажимаю кнопку 'Регистрация'")
     def submit_login(self):
         self.click(self.REGISTER_BUTTON)
+        allure.attach("Кликнули по кнопке 'Регистрация'")
 
+    @allure.step("Ищу подтверждение регистрации")
     def is_account_created(self):
         return self.wait.until(lambda x: x.title == "Your Account Has Been Created!")
